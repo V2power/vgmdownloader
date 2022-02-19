b62e301731160cd61fa334dc45715a3ac57dc988
Vitor <vitor5088@gmail.com>
2022-02-19 20:14:14 -0300

    Adicionado o sistema de criação de pasta de acordo com o titulo

--- vgmdownloader.py
+++ vgmdownloader.py
@@ -1,9 +1,7 @@
 import os, requests
-from re import S
 from urllib import response
 from bs4 import BeautifulSoup
 
-os.chdir("D:/Music")
 r = requests.get("https://downloads.khinsider.com/")
 baseURL = str(r.url)
 # zelda
@@ -14,6 +12,8 @@ searchPage = requests.get(baseURL + "/search?search=" + gameName)
 lista = []
 searchSoup = BeautifulSoup(searchPage.text, "html.parser")
 search = searchSoup.find("div", {"id": "EchoTopic"})
+
+# Apresentando as opções de álbuns para download
 i = 0
 for link in search.find_all('a'):
     games = link.get('href')
@@ -23,12 +23,30 @@ for link in search.find_all('a'):
 n = int(input("Select the album number you want to download: "))
 n = n - 1
 gameLink = lista[n]
+
+# Entrando na página do álbum selecionado
 downres = requests.get(str(baseURL + gameLink))
 downSoup = BeautifulSoup(downres.text, "html.parser")
 echoTopic = downSoup.find("div", {"id": "EchoTopic"})
 songList = echoTopic.find("table", {"id": "songlist"})
+
+# Criando uma pasta com o título do álbum para salvar as músicas
+albumTitle = echoTopic.find("h2").string
+directory = albumTitle
+parent_dir = "D:/Music/"
+path = os.path.join(parent_dir, directory)
+mode = 0o666
+try:
+    os.mkdir(path, mode)
+    print("Folder '% s' created" % directory)
+    print("Salving the downloaded song in this folder!")
+    fullDirectory = parent_dir + directory
+    os.chdir(fullDirectory)
+except OSError as error:
+    print(error)
+
+# Selecionando as músicas para baixar
 for link in songList.find_all('a'):
-    # songName = link.string
     songs = link.get('href')
     response = requests.get(str(baseURL+songs))
     songSoup = BeautifulSoup(response.text, "html.parser")
@@ -36,28 +54,34 @@ for link in songList.find_all('a'):
     songNamePlace = songDown.find_next("p", {"align": "left"})
     songNamePlace2 = songNamePlace.find_next("p", {"align": "left"})
     songName = songNamePlace2.find_next('b')
-    songName2 = songName.find_next('b').string
-    print(songName2)
+    songTitle = songName.find_next('b').string
+    print("Download this song: " + songTitle)
     songFile = songDown.find_all("a", {"style": "color: #21363f;"})
+
+    # Deixando a conexão aberta para downloads multiplos
     with requests.Session() as req:
+        # Selecionando os links para entrar na música
         for link in songFile:
-            songs2 = link.get('href')
-            href = songs2
-            response2 = requests.get(str(baseURL+songs2))
+            songLink = link.get('href')
+            href = songLink
+            status = requests.get(str(baseURL+songLink))
             mp3 = "mp3"
             flac = "flac"
-            print(songs2)
-            print(response2)
-            print("Downloading song... please wait...")
+            print("Downloading from this URL: " + songLink)
+            print(status)
+            print("Downloading... ")
             download = req.get(href)
             if download.status_code == 200:
                 if flac in href:
-                    with open(songName2+ ".flac", 'wb') as f:
+                    with open(songTitle+ ".flac", 'wb') as f:
                         f.write(download.content)
                         print("Done downloading!")
                 else:
-                    with open(songName2+ ".mp3", 'wb') as f:
+                    with open(songTitle+ ".mp3", 'wb') as f:
                         f.write(download.content)
                         print("Done downloading!")
             else:
                 print("ERROR, song could not be downloaded!")
+        print("Selecting the next song")
+    print("No more songs found!")
+print("Thanks for using this program!")
